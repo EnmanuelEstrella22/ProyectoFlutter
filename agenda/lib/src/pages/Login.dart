789234b3ce73a681dc5e85a3c3541d6c88977f55
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2019-11-14 17:35:11 -0400

    Terminando el login y el registrar, solo des falta la validacion

--- agenda/lib/src/pages/Login.dart
+++ agenda/lib/src/pages/Login.dart
@@ -10,7 +10,6 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
-
   final TextStyle estilo = new TextStyle(fontSize: 20.0);
 
   Widget build(BuildContext context) {
@@ -20,296 +19,330 @@ class _LoginState extends State<Login> {
           _fondoApp(),
           ListView(
             children: <Widget>[
-             _formulario(),
+              _formulario(),
               SizedBox(
-              height: 200.0,
-              width: double.infinity,
-            ), 
-             _formularioRegistro()
+                height: 200.0,
+                width: double.infinity,
+              ),
+              _formularioRegistro()
             ],
           )
-          
         ],
       ),
     );
   }
- 
- //fondo del login
- Widget  _fondoApp() {
 
-   final gradiente = Container(
-     width: double.infinity,
-     height: double.infinity,
-     decoration: BoxDecoration(
-       color: Colors.white 
-     ),
-   );
+  //fondo del login
+  Widget _fondoApp() {
+    final gradiente = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(color: Colors.white),
+    );
 
-   final caja = Transform.rotate(
-     angle: -pi / 5.0,
-     child:Container(
-       height: 360.0,
-       width: 360.0,
-       decoration: BoxDecoration(
-         borderRadius: BorderRadius.circular(300.0),
-        color: Color.fromARGB(150, 245, 117, 22 )
-       ),
-     ),
-   );
-   final caja1 = Transform.rotate(
-     angle: -pi / 5.0,
-     child:Container(
-       height: 160.0,
-       width: 160.0,
-       decoration: BoxDecoration(
-         borderRadius: BorderRadius.circular(10.0),
-         color: Color.fromARGB(150,250, 216, 191 )         
-       ),
-     ),
-   );
-   final caja3 = Transform.rotate(
-     angle: -pi / 5.0,
-     child:Container(
-       height: 250.0,
-       width: 250.0,
-       decoration: BoxDecoration(
-         borderRadius: BorderRadius.circular(300.0),
-        color: Color.fromARGB(150, 245, 117, 22 )
-       ),
-     ),
-   );    
-   final caja4 = Transform.rotate(
-     angle: -pi / 5.0,
-     child:Container(
-       height: 150.0,
-       width: 150.0,
-       decoration: BoxDecoration(
-         borderRadius: BorderRadius.circular(300.0),
-        color: Color.fromARGB(150, 252, 250, 249)
-       ),
-     ),
-   );       
-   final caja6 = Transform.rotate(
-     angle: -pi / 5.0,
-     child:Container(
-       height: 150.0,
-       width: 150.0,
-       decoration: BoxDecoration(
-         borderRadius: BorderRadius.circular(300.0),
-        color: Color.fromARGB(150, 252, 250, 249)
-       ),
-     ),
-   );      
-   return Stack(
-     children: <Widget>[
-       gradiente,
-       Positioned(
-         top: -130.0,
-         child: caja,
-       ),
-       Positioned(
-         top: -40.0,
-         right: 100.0,
-         child: caja1,
-       ),
-       Positioned(
-         bottom: -100.0,
-         left: 150.0,
-         child: caja3,
-       ),
-       Positioned(
-         bottom: -30.0,
-         left: 130.0,
-         child: caja4,
-       ),
-       Positioned(
-         bottom: -50.0,
-         left: 250.0,
-         child: caja6,
-       ),
-     ],
-   );
- }
-
- //formulario de inicio de secion
- Widget _formulario() {
-
-   final contenedor = Container(
-     margin: EdgeInsets.only(top: 20.0,left: 15.0),
-     padding: EdgeInsets.only(right: 10.0,left: 10.0) ,
-     width: 330.0,
-     height: 500.0,
-     decoration: BoxDecoration(
-       borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0),
-       topRight: Radius.circular(50.0),bottomLeft: Radius.circular(50.0),
-       bottomRight: Radius.circular(10.0)),
-       color: Colors.white,
-       boxShadow: <BoxShadow>[
-        BoxShadow(
-            color: Colors.black26,
-            blurRadius: 3.0,
-            offset: Offset(0.2, 0.5),
-            spreadRadius: 4.0)
+    final caja = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(300.0),
+            color: Color.fromARGB(150, 245, 117, 22)),
+      ),
+    );
+    final caja1 = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 160.0,
+        width: 160.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Color.fromARGB(150, 250, 216, 191)),
+      ),
+    );
+    final caja3 = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 250.0,
+        width: 250.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(300.0),
+            color: Color.fromARGB(150, 245, 117, 22)),
+      ),
+    );
+    final caja4 = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 150.0,
+        width: 150.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(300.0),
+            color: Color.fromARGB(150, 252, 250, 249)),
+      ),
+    );
+    final caja6 = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 150.0,
+        width: 150.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(300.0),
+            color: Color.fromARGB(150, 252, 250, 249)),
+      ),
+    );
+    return Stack(
+      children: <Widget>[
+        gradiente,
+        Positioned(
+          top: -130.0,
+          child: caja,
+        ),
+        Positioned(
+          top: -40.0,
+          right: 100.0,
+          child: caja1,
+        ),
+        Positioned(
+          bottom: -100.0,
+          left: 150.0,
+          child: caja3,
+        ),
+        Positioned(
+          bottom: -30.0,
+          left: 130.0,
+          child: caja4,
+        ),
+        Positioned(
+          bottom: -50.0,
+          left: 250.0,
+          child: caja6,
+        ),
       ],
-     ),
-     child: Column(
-       crossAxisAlignment: CrossAxisAlignment.center,
-       children: <Widget>[
-         Icon(
-           Icons.account_circle,
-           color: Colors.orange,
-           size: 80.0,
-         ),
-         Text("LOGIN",style: TextStyle(color: Colors.orangeAccent,fontSize: 25.0, fontWeight: FontWeight.normal),),
-          SizedBox(height: 40.0,width: double.infinity),
-         _correoTexto(),
-          SizedBox(height: 20.0,width: double.infinity),
-         _passTexto(),
-          SizedBox(height: 40.0,width: double.infinity),
-         _crearBoton(),
-          SizedBox(height: 10.0,width: double.infinity),
-            _googleBotton(), 
-            // SizedBox(width: 100.0),
-           _facebookBotton()
-         
-         
+    );
+  }
 
+  //formulario de inicio de secion
+  Widget _formulario() {
+    final contenedor = Container(
+      margin: EdgeInsets.only(top: 20.0, left: 15.0),
+      padding: EdgeInsets.only(right: 10.0, left: 10.0),
+      width: 330.0,
+      height: 500.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(50.0),
+            bottomLeft: Radius.circular(50.0),
+            bottomRight: Radius.circular(10.0)),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black26,
+              blurRadius: 3.0,
+              offset: Offset(0.2, 0.5),
+              spreadRadius: 4.0)
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.account_circle,
+            color: Colors.orange,
+            size: 80.0,
+          ),
+          Text(
+            "LOGIN",
+            style: TextStyle(
+                color: Colors.orangeAccent,
+                fontSize: 25.0,
+                fontWeight: FontWeight.normal),
+          ),
+          SizedBox(height: 40.0, width: double.infinity),
+          _correoTexto(),
+          SizedBox(height: 20.0, width: double.infinity),
+          _passTexto(),
+          SizedBox(height: 40.0, width: double.infinity),
+          _crearBoton(),
+          SizedBox(height: 10.0, width: double.infinity),
+          _googleBotton(),
+          // SizedBox(width: 100.0),
+          _facebookBotton()
+        ],
+      ),
+    );
 
+    return Stack(
+      children: <Widget>[
+        contenedor,
+      ],
+    );
+  }
 
-
-       ],
-     ),
-   );
-
-   return Stack(
-     children: <Widget>[
-       contenedor,
-     ],
-   );
- }
   //formulario de registro
   Widget _formularioRegistro() {
+    final contenedor = Container(
+      margin: EdgeInsets.only(top: 20.0, left: 15.0,bottom: 30.0),
+      padding: EdgeInsets.only(right: 10.0, left: 10.0),
+      width: 330.0,
+      height: 500.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50.0),
+            topRight: Radius.circular(10.0),
+            bottomLeft: Radius.circular(10.0),
+            bottomRight: Radius.circular(50.0)),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black26,
+              blurRadius: 3.0,
+              offset: Offset(0.2, 0.5),
+              spreadRadius: 4.0)
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.account_circle,
+            color: Colors.orange,
+            size: 80.0,
+          ),
+          Text(
+            "SIGN IN",
+            style: TextStyle(
+                color: Colors.orangeAccent,
+                fontSize: 25.0,
+                fontWeight: FontWeight.normal),
+          ),
+          SizedBox(height: 40.0, width: double.infinity),
+          _correoTexto(),
+          SizedBox(height: 20.0, width: double.infinity),
+          _passTexto(),
+          SizedBox(height: 20.0, width: double.infinity),
+          _passTextoConfir(),
+          SizedBox(height: 40.0, width: double.infinity),
+          _crearBotonRegis(),
 
-   final contenedor = Container(
-     margin: EdgeInsets.only(bottom: 30.0,left: 30.0),
-     width: 300.0,
-     height: 500.0,
-     decoration: BoxDecoration(
-       borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0),
-       topRight: Radius.circular(50.0),bottomLeft: Radius.circular(50.0),
-       bottomRight: Radius.circular(10.0)),
-       color: Colors.white,
-       boxShadow: <BoxShadow>[
-        BoxShadow(
-            color: Colors.black26,
-            blurRadius: 3.0,
-            offset: Offset(0.2, 0.5),
-            spreadRadius: 4.0)
-      ],
-     ),
-   );
+        ],
+      ),
+    );
 
-   return Stack(
-     children: <Widget>[
-       contenedor
-     ],
-   );
- }
+    return Stack(
+      children: <Widget>[contenedor],
+    );
+  }
 
   //input Correo
   Widget _correoTexto() {
-  return TextField(
-    //autofocus: true,
-    textCapitalization: TextCapitalization.characters,
-    decoration: InputDecoration(
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-      labelText: 'Correo',
-      labelStyle: estilo,
-      hintText: 'Escribe aqui',
-      icon: Icon(
-        Icons.account_circle,
-        color: Colors.orange,
-        size: 35.0,
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      cursorColor: Colors.orangeAccent,
+      cursorRadius: Radius.circular(5.0),
+      cursorWidth: 3.0,
+      decoration: InputDecoration(
+         prefixIcon: Icon(
+          Icons.account_circle,
+          color: Colors.orange,
+          size: 35.0,
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        labelText: 'Correo',
+        labelStyle: estilo,
+        hintText: 'Escribe aqui',
+       
       ),
-    ),
-  );
- }
-
- //Input contrasena
- Widget _passTexto() {
-  return TextField(
-    //autofocus: true,
-    textCapitalization: TextCapitalization.characters,
-    decoration: InputDecoration(
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-      labelText: 'Contraseña',
-      labelStyle: estilo,
-      hintText: 'Escribe aqui',
-      icon: Icon(
-        Icons.lock,
-        color: Colors.orange,
-        size: 35.0,
-      ),
-    ),
-  );
+    );
   }
 
+  //Input contrasena
+  Widget _passTexto() {
+    return TextField(
+      cursorColor: Colors.orangeAccent,
+      cursorRadius: Radius.circular(5.0),
+      cursorWidth: 3.0,
+      obscureText: true,
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.lock,
+          color: Colors.orange,
+          size: 35.0,
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        labelText: 'Contraseña',
+        labelStyle: estilo,
+        hintText: 'Escribe aqui',
+      ),
+    );
+  }
 
-
-
+ //Confirmar clave
+  Widget _passTextoConfir() {
+    return TextField(
+      cursorColor: Colors.orangeAccent,
+      cursorRadius: Radius.circular(5.0),
+      cursorWidth: 3.0,
+      obscureText: true,
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.lock,
+          color: Colors.orange,
+          size: 35.0,
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        labelText: 'Confirmar Contraseña',
+        labelStyle: estilo,
+        hintText: 'Escribe aqui',
+      ),
+    );
+  }
 
 //BOTONES
 
-
   //Boton de iniciar secion
   Widget _crearBoton() {
-    return  RaisedButton(
-      padding: EdgeInsets.only(left: 35.0,right: 35.0),
-        shape:  RoundedRectangleBorder(
-            borderRadius:  BorderRadius.circular(18.0),
-            side: BorderSide(color: Colors.orangeAccent)),
-        onPressed: () {
-            Navigator.pushNamed(context, 'Menu');
-        },
-        color: Colors.white,
-        textColor: Colors.orangeAccent,
-        child: Text("Iniciar".toUpperCase(),
-            style: TextStyle(fontSize: 20)),
-      );
+    return RaisedButton(
+      padding: EdgeInsets.only(left: 35.0, right: 35.0),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          side: BorderSide(color: Colors.orangeAccent)),
+      onPressed: () {
+        Navigator.pushNamed(context, 'Menu');
+      },
+      color: Colors.white,
+      textColor: Colors.orangeAccent,
+      child: Text("Iniciar".toUpperCase(), style: TextStyle(fontSize: 20)),
+    );
   }
 
- Widget _googleBotton() {
-   return GoogleSignInButton(
-     onPressed: () {},
-     borderRadius:50.0 ,
-     );
- }
+  //Boton Registrarte
+    Widget _crearBotonRegis() {
+    return RaisedButton(
+      padding: EdgeInsets.only(left: 35.0, right: 35.0),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          side: BorderSide(color: Colors.orangeAccent)),
+      onPressed: () {
+        Navigator.pushNamed(context, 'Menu');
+      },
+      color: Colors.white,
+      textColor: Colors.orangeAccent,
+      child: Text("INICIAR".toUpperCase(), style: TextStyle(fontSize: 20)),
+    );
+  }
 
- Widget _facebookBotton(){
-   return FacebookSignInButton(
-     onPressed: () {}
-   ,borderRadius:50.0 ,
-  //  text: "FACEBOOK",
-   );
-  //  return Go
-  // return   FlatButton(
-  //     shape: new RoundedRectangleBorder(
-  //           borderRadius: new BorderRadius.circular(18.0),
-  //           side: BorderSide(color: Colors.red)),
-  //       color: Colors.white,
-  //       textColor: Colors.red,
-  //       padding: EdgeInsets.all(0.0),
-        
-  //       onPressed: () {},
-  //       child: Text(
-  //         "F".toUpperCase(),
-  //         style: TextStyle(
-  //           fontSize: 14.0,
-  //         ),
-  //       ),
-  // );
+  Widget _googleBotton() {
+    return GoogleSignInButton(
+      onPressed: () {},
+      borderRadius: 50.0,
+    );
+  }
+
+  Widget _facebookBotton() {
+    return FacebookSignInButton(
+      onPressed: () {}, borderRadius: 50.0,
+      //  text: "FACEBOOK",
+    );
+  }
 }
-
-}
-
-
